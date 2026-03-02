b940b4549d39ea42170123337c4b1da230408be0
Ajit Kumar <ajitakumarbehera64@gmail.com>
2026-03-02 22:39:23 +0530

    Initial commit

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ server.use(jsonServer.bodyParser);
 server.use(router);
 
 // IMPORTANT: use 0.0.0.0 instead of localhost
-server.listen(3000, "0.0.0.0", () => {
-  console.log("Backend running on http://192.168.1.5:3000");
+// server.listen(3000, "0.0.0.0", () => {
+//   console.log("Backend running on http://192.168.1.5:3000");
+// });
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
