45c009a1af5604905d81400d17d04a4453663099
Ajit Kumar <ajitakumarbehera64@gmail.com>
2026-03-02 22:48:05 +0530

    Fixed server listen issue

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+const PORT = process.env.PORT || 3000;
+
+// Use middlewares
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.use(router);
 
-// IMPORTANT: use 0.0.0.0 instead of localhost
-// server.listen(3000, "0.0.0.0", () => {
-//   console.log("Backend running on http://192.168.1.5:3000");
-// });
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+// Listen properly for Render
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
